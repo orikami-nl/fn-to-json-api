714d145d6eb6ee7d230e975431eeda2eb9b80660
Artur Wojnar <artur.wojnar@revolve.pro>
2021-04-06 11:01:53 +0200

    MS-2730 Taking the token aither from URL or HEADER but never from BODY to make sure the token got verified

--- index.test.js
+++ index.test.js
@@ -69,6 +69,72 @@ test("200 GET async function", async () => {
   });
 });
 
+test("200 POST with bearer token passed within the header", async () => {
+  expect.assertions(2);
+  const res = await request({
+    method: "POST",
+    url: "/?token=URL",
+    body: "token=BODY",
+    headers: {
+      'Authorization': 'bearer HEADER'
+    },
+    handler: toJsonApi(echo)
+  });
+
+  expect(res.statusCode).toBe(200);
+  // If the token is in the Authorization header, then it takes a precedence, the same as in the micro-auth0 package.
+  // The body token is ignored.
+  expect(JSON.parse(res.body)).toEqual({
+    token: 'HEADER'
+  });
+});
+
+test("200 POST with bearer token passed within only the header", async () => {
+  expect.assertions(2);
+  const res = await request({
+    method: "POST",
+    url: "/",
+    headers: {
+      'Authorization': 'bearer HEADER'
+    },
+    handler: toJsonApi(echo)
+  });
+
+  expect(res.statusCode).toBe(200);
+  expect(JSON.parse(res.body)).toEqual({
+    token: 'HEADER'
+  });
+});
+
+test("200 POST with bearer token passed within the query param", async () => {
+  expect.assertions(2);
+  const res = await request({
+    method: "POST",
+    url: "/?token=URL",
+    body: "token=BODY",
+    handler: toJsonApi(echo)
+  });
+
+  // If token is in both URL and BODY, then the latter is ignored.
+  expect(res.statusCode).toBe(200);
+  expect(JSON.parse(res.body)).toEqual({
+    token: 'URL'
+  });
+});
+
+test("200 POST with bearer token passed within only the body", async () => {
+  expect.assertions(2);
+  const res = await request({
+    method: "POST",
+    body: "token=BODY",
+    handler: toJsonApi(echo)
+  });
+
+  expect(res.statusCode).toBe(200);
+  // Body token is omitted and ignored.
+  expect(JSON.parse(res.body)).toEqual({});
+});
+
 const echo = data => data
 
 const echoAsync = async data => {
